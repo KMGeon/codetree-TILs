cd0d73e775bb3c2a63be90a49d54fb441298dcda
김무건 <pos04167@kakao.com>
2024-02-26 21:44:12 +0900

    Title: 최소공배수 구하기, Time: 146ms, Memory: 10MB, Status: Passed - CodeTree

--- 240226/최소공배수 구하기/find-the-least-common-multiple.java
+++ 240226/최소공배수 구하기/find-the-least-common-multiple.java
@@ -25,3 +25,28 @@ public class Main {
 
     }
 }
+/**
+- 정답 코드
+
+    // n과 m의 최소공배수를 출력합니다.
+    public static void findLCM(int n, int m) {
+        int gcd = 0;
+        for(int i = 1; i <= Math.min(n, m); i++) {
+            if(n % i == 0 && m % i == 0)
+                gcd = i;
+        }
+    
+        System.out.print(n * m / gcd);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 변수 선언 및 입력:
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        findLCM(n, m);
+    }
+}
+
+*/

8cbc2a1343718046c59151b062e100c50928a8e6
김무건 <pos04167@kakao.com>
2024-03-22 17:03:17 +0900

    Title: 사칙연산 함수, Time: 120ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240322/사칙연산 함수/quadratic-operations-function.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        char o = sc.next().charAt(0);
+        int c = sc.nextInt();
+
+        System.out.println(Main.solution(a, o, c));
+    }
+
+    private static String solution(int a, char o, int c) {
+        String result = "";
+        List<String> list = new ArrayList<>();
+
+        if (o != '+' && o != '-' && o != '*' && o != '/') {
+            return "false";
+        }
+
+        switch (o) {
+            case '+':
+                result = String.valueOf(a) + " " + o + " " + String.valueOf(c) + " " + "=" + " " + String.valueOf(a + c);
+                break;
+            case '-':
+                result = String.valueOf(a) + " " + o + " " + String.valueOf(c) + " " + "=" + " " + String.valueOf(a - c);
+                break;
+            case '*':
+                result = String.valueOf(a) + " " + o + " " + String.valueOf(c) + " " + "=" + " " + String.valueOf(a * c);
+                break;
+            case '/':
+                result = String.valueOf(a) + " " + o + " " + String.valueOf(c) + " " + "=" + " " +  String.valueOf(a / c);
+                break;
+        }
+        return result;
+    }
+}

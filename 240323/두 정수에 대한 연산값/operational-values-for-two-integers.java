fe781e63a8997b5154b81691d5db5bff35ad3b64
김무건 <pos04167@kakao.com>
2024-03-23 12:32:36 +0900

    Title: 두 정수에 대한 연산값, Time: 123ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240323/두 정수에 대한 연산값/operational-values-for-two-integers.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+
+		System.out.println(Main.solution(a, b));
+	}
+
+	private static String solution(int a, int b) {
+		return a > b ? logic(a, b) : logic(b, a);
+	}
+
+	private static String logic(int a, int b) {
+		return (a + 25) + " " + (b * 2);
+	}
+}

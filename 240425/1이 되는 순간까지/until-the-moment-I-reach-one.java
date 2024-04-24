66535c63374777dfaa11bcefa5f55fb9932ed7db
김무건 <pos04167@kakao.com>
2024-04-25 00:38:22 +0900

    Title: 1이 되는 순간까지, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240425/1이 되는 순간까지/until-the-moment-I-reach-one.java
@@ -0,0 +1,26 @@
+public class Main {
+    private static Integer result =0;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        System.out.println(Main.solution(n));
+    }
+
+    private static Integer solution(int n) {
+
+        if (n == 1) {
+            return result;
+        }
+
+        if (n % 2 == 0) {
+            result ++;
+            solution(n/2);
+        } else {
+            result ++;
+            solution(n/3);
+        }
+
+        return result;
+    }
+}

d241452d06a9d243673b343215e75a70936bfb39
김무건 <pos04167@kakao.com>
2024-05-16 19:39:25 +0900

    Title: 단어 정렬, Time: 165ms, Memory: 10MB, Status: Passed - CodeTree

--- 240516/단어 정렬/sorting-words.java
+++ 240516/단어 정렬/sorting-words.java
@@ -1,38 +1,20 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a1 = sc.nextInt();
-        int b2 = sc.nextInt();
+        
+        // 입력
+        int n = sc.nextInt();
+        String[] wordList = new String[n];
+        for(int i = 0; i < n; i++)
+            wordList[i] = sc.next();
 
-        String str = sc.next();
+        // 문자열 사전순 정렬
+        Arrays.sort(wordList);
 
-
-        String[] strArr = new String[a1];
-        for (int i = 0; i < a1; i++) {
-            strArr[i] = sc.next();
-        }
-
-
-        List<String> list = new ArrayList<>();
-
-        for (String s : strArr) {
-            String substring = s.substring(0, 2);
-
-            if (substring.equals(str)){
-                list.add(s);
-            }
-        }
-
-        // list에 있는 데이터를 string배열로 만들기
-        String[] resultArr = list.toArray(new String[0]);
-
-        Arrays.sort(resultArr);
-
-        System.out.println(resultArr[3]);
+        for(int i = 0; i < n; i++)
+            System.out.println(wordList[i]);
     }
 }

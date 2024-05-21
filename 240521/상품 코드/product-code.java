895bf75c96fc408f66b8f13f94354b8bf8844081
김무건 <pos04167@kakao.com>
2024-05-21 22:45:33 +0900

    Title: 상품 코드, Time: 154ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240521/상품 코드/product-code.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class Product {
+    String id;
+    int code;
+
+    public Product(String id, int code) {
+        this.id = id;
+        this.code = code;
+    }
+};
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Product product1 = new Product("codetree", 50);
+        
+        String id2 = sc.next();
+        int code2 = sc.nextInt();
+        
+        Product product2 = new Product(id2, code2);
+
+        System.out.println("product " + product1.code + " is " + product1.id);
+        System.out.println("product " + product2.code + " is " + product2.id);
+    }
+}

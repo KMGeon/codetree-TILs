2e3f4a41465d9fcfcf93b522d30925f203f3be4a
김무건 <pos04167@kakao.com>
2024-05-23 22:18:06 +0900

    Title: 사는 지역, Time: 137ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240523/사는 지역/where-live.java
+++ 240523/사는 지역/where-live.java
@@ -18,7 +18,7 @@ public class Main {
 
         int TEMP =0;
 
-        for (int i=0; i< number; i++){
+        for (int i=1; i< number; i++){
             if ((personArr[i].getName().compareTo(personArr[TEMP].getRegion() )> 0)) {
                 TEMP = i;
             }

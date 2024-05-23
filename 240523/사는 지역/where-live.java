08be21ee1e2947cc1b0f5be6e640d27ed52a3f4d
김무건 <pos04167@kakao.com>
2024-05-23 22:15:40 +0900

    Title: 사는 지역, Time: 124ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240523/사는 지역/where-live.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+
+        Person[] personArr = new Person[number];
+
+
+        for (int i = 0; i < number; i++) {
+            String name = sc.next();
+            String address = sc.next();
+            String region = sc.next();
+            personArr[i] = new Person(name, address, region);
+        }
+
+        int TEMP =0;
+
+        for (int i=0; i< number; i++){
+            if ((personArr[i].getName().compareTo(personArr[TEMP].getRegion() )> 0)) {
+                TEMP = i;
+            }
+        }
+
+        System.out.println("name "+personArr[TEMP].getName());
+        System.out.println("addr "+personArr[TEMP].getAddress());
+        System.out.println("city "+personArr[TEMP].getRegion());
+
+    }
+
+}
+
+class Person {
+    private String name;
+    private String address;
+    private String region;
+
+    public Person(String name, String address, String region) {
+        this.name = name;
+        this.address = address;
+        this.region = region;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+}

e310c6ccb7e8450359e0ab53260e811288d9f96e
김무건 <pos04167@kakao.com>
2024-06-11 21:59:37 +0900

    Title: 최고의 13위치, Time: 161ms, Memory: 11MB, Status: Passed - CodeTree

--- 240611/최고의 13위치/best-place-of-13.java
+++ 240611/최고의 13위치/best-place-of-13.java
@@ -5,6 +5,7 @@ public class Main {
 
     public static int n;
     public static int[][] arr = new int[MAX_N][MAX_N];
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -13,16 +14,13 @@ public class Main {
             for(int j = 0; j < n; j++)
                 arr[i][j] = sc.nextInt();
 
-
-        int maxVal = 0;
-        for (int i=0; i<n; i++){
-            for (int j=i; j<n-2; j++){
-                int cnt = arr[i][j] + arr[i][j+1] + arr[i][j+2];
-                maxVal = Math.max(maxVal, cnt);
-
+        int maxCnt = 0;
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n - 2; j++) {
+                int cnt = arr[i][j] + arr[i][j + 1] + arr[i][j + 2];
+                maxCnt = Math.max(maxCnt, cnt);
             }
-        }
-
-        System.out.print(maxVal);
+        
+        System.out.println(maxCnt);
     }
 }

9e9b6be42386faae7ccd8036a8406441f680eaa1
김무건 <pos04167@kakao.com>
2024-06-27 20:19:29 +0900

    Title: 구간 중 최대 합, Time: 166ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240627/구간 중 최대 합/max-sum-of-subarray.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static final int MAX_N = 100;
+
+    public static int n, k;
+    public static int[] arr = new int[MAX_N];
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        k = sc.nextInt();
+        for(int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+
+        int maxSum = 0;
+        for(int i = 0; i <= n - k; i++) {
+            int sum = 0;
+            for(int j = i; j < i + k; j++)
+                sum += arr[j];
+            
+            maxSum = Math.max(maxSum, sum);
+        }
+        
+        System.out.println(maxSum);
+    }
+}

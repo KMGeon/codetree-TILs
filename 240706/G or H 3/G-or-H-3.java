58fcd16945b202fdd2b7a48c50cba2daa75882a1
김무건 <pos04167@kakao.com>
2024-07-06 20:22:27 +0900

    Title: G or H 3, Time: 158ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240706/G or H 3/G-or-H-3.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+    public static final int MAX_NUM = 10000;
+
+    public static int n, k;
+    public static int[] arr = new int[MAX_NUM + 1];
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        k = sc.nextInt();
+        for(int i = 0; i < n; i++) {
+            int x = sc.nextInt();
+            char c = sc.next().charAt(0);
+            
+            if(c == 'G')
+                arr[x] = 1;
+            else
+                arr[x] = 2;
+        }
+        
+        int maxSum = 0;
+        for(int i = 0; i <= MAX_NUM - k; i++) {
+            // 해당 구간 내 원소의 합
+            int sum = 0;
+            for(int j = i; j <= i + k; j++)
+                sum += arr[j];
+            
+            maxSum = Math.max(maxSum, sum);
+        }
+        
+        System.out.println(maxSum);
+    }
+}

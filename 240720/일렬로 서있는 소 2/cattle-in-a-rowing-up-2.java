ab5eb7198b2d809f4977e5d981b2cadc382780a8
김무건 <pos04167@kakao.com>
2024-07-20 17:31:12 +0900

    Title: 일렬로 서있는 소 2, Time: 146ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240720/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int[] arr = new int[num];
+
+        for (int i = 0; i < num; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int result = 0;
+        for (int i = 0; i < num - 2; i++) {
+            for (int j=i+1; j<num; j++){
+                for (int k=j+1; k<num; k++){
+                    if (isIjk(i, j, k) && isValueIjk(i,j,k, arr)){
+                        result ++;
+                    }
+                }
+            }
+        }
+
+        System.out.println(result);
+
+
+    }
+
+    private static boolean isValueIjk(int i, int j, int k, int[] arr) {
+        return arr[i] <arr[j] && arr[j] < arr[k] ? true : false;
+    }
+
+    private static boolean isIjk(int i, int j, int k) {
+        return i<j && j<k ? true : false;
+
+    }
+}

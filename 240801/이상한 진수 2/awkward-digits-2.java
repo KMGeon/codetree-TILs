16af06e9b27406443ad90b251e4ff3da43d1c4c1
김무건 <pos04167@kakao.com>
2024-08-01 00:31:55 +0900

    Title: 이상한 진수 2, Time: 140ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240801/이상한 진수 2/awkward-digits-2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    private static int INPUT = 1010;
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String a = "1010";
+
+        int maxN = 0;
+
+        for (int i = 0; i < a.length(); i++) {
+            StringBuilder modified = new StringBuilder(a);
+            if (a.charAt(i) == '0') {
+                modified.setCharAt(i, '1');
+            } else {
+                modified.setCharAt(i, '0');
+            }
+
+            int current = Integer.parseInt(modified.toString(), 2);
+            maxN = Math.max(maxN, current);
+        }
+        System.out.println(maxN);
+    }
+}

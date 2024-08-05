6fe40d37dcc4d8a8ba6f8726e39c67e07439e7bd
김무건 <pos04167@kakao.com>
2024-08-06 00:25:44 +0900

    Title: 마라톤 중간에 택시타기 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240806/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.java
@@ -0,0 +1,33 @@
+public class Main {
+    public static final int INT_MAX = Integer.MAX_VALUE;
+    public static final int MAX_N = 100;
+
+    public static int n;
+    public static int[] x = new int[MAX_N];
+    public static int[] y = new int[MAX_N];
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 변수 선언 및 입력
+        n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            x[i] = sc.nextInt();
+            y[i] = sc.nextInt();
+        }
+
+        int result = 0;
+        for (int i = 1; i < n - 1; i++) {
+
+            int diff = 0;
+            int prev = 0;
+            for (int j = 1; j < n; j++) {
+                if (i==j) continue;
+                diff = Math.abs(x[prev] - x[j]) + Math.abs(y[prev] - y[j]);
+                prev = j;
+            }
+            result = Math.min(diff, result);
+        }
+        System.out.println(result);
+
+    }
+}
